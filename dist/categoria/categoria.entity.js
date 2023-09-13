489d3264185c6fcce101dd26e218e8a553f72e47
Emiliano Gamba <emilianofranciscogamba@outlook.com>
2023-09-13 20:19:12 -0300

    borre carpeta de categoria que era de ejemplo

--- dist/categoria/categoria.entity.js
+++
@@ -1,8 +0,0 @@
-import crypto from 'node:crypto';
-export class categoria {
-    constructor(nombre, id = crypto.randomUUID()) {
-        this.nombre = nombre;
-        this.id = id;
-    }
-}
-//# sourceMappingURL=categoria.entity.js.map
